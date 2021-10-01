7df50177680a14f2efd4b7975059ccfc8c94d749
tamayegg <91484967+tamayegg@users.noreply.github.com>
2021-10-01 13:28:17 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="timechecker",
+    version="0.0.1",
+    description="An application that informs you of the time in different locations and timezones",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["click", "pytz"],
+    entry_points={"console_scripts": ["timechecker = src.main:main"]},
+)
